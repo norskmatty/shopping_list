2ef6616f2814ce1b13d89e7dc09316ede1228635
Matthew Conley <matthewconley00@gmail.com>
2016-08-09 09:06:55 +0000

    new config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || (process.env.NODE_ENV === 'production' ?  'mongodb://localhost/shopping-list' : 'mongodb://localhost/shopping-list-dev');
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || (process.env.NODE_ENV === 'production' ?  'mongodb://norskmatty:password@ds145325.mlab.com:45325/shopping-list');
 exports.PORT = process.env.PORT || 8080;
